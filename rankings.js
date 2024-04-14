9a3d5d94b4f18f289100eec03627c38130d0132e
donutdiet <72674098+donutdiet@users.noreply.github.com>
2024-04-13 21:22:42 -0400

    Implement collapsible sidebar menu

--- rankings.js
+++ rankings.js
@@ -1,11 +1,17 @@
+/* ----------------------------------------------
+Light/dark mode toggle button
+- default light mode since I think it looks 
+better (will implement system default later)
+- 
+------------------------------------------------- */
+
 setTheme("light");
 
 document.querySelector(".toggle-theme-button").addEventListener("click", toggleTheme);
 
 function toggleTheme() {
     const root = document.documentElement;
-    const newTheme = root.className == "dark" ? "light" : "dark";
-    root.className = newTheme;
+    root.className == "dark" ? setTheme("light") : setTheme("dark");
 
     document.querySelector(".toggle-theme-button").textContent = newTheme.charAt(0).toUpperCase() + newTheme.slice(1) + " Mode";
 }
@@ -14,4 +20,28 @@ function setTheme(theme) {
     const root = document.documentElement;
     root.className = theme;
     document.querySelector(".toggle-theme-button").textContent = theme.charAt(0).toUpperCase() + theme.slice(1) + " Mode";
+}
+
+/* ----------------------------------------------
+Collapsible Sidebar Menu
+- can't use "this" keyword in arrow function
+- 
+------------------------------------------------- */
+
+const collapsibleMenus = document.querySelectorAll(".collapsible");
+
+for(let i=0; i<collapsibleMenus.length; i++) {
+    collapsibleMenus[i].addEventListener("click", collapseMenu);
+}
+
+function collapseMenu() {
+    const tabs = this.nextElementSibling;
+    const symbol = this.querySelector("span");
+    if(tabs.style.display === "block") {
+        tabs.style.display = "none";
+        symbol.textContent = "+";
+    } else {
+        tabs.style.display = "block";
+        symbol.textContent = "-";
+    }
 }
