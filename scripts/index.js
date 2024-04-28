ae17e7aba65b1c81f2418dc03e3b4f354cc03af7
donutdiet <72674098+donutdiet@users.noreply.github.com>
2024-04-27 23:30:27 -0400

    Fix bug so sign up button works on first click

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
 /* ----------------------------------------------
 SIGN-UP FORM TOGGLE BUTTON
-- 
+- setting display: none returns an empty string when trying to access the style for the first time idk why
 ------------------------------------------------- */
 const signUpButton = document.querySelector(".sign-up");
 const signUpForm = document.querySelector(".sign-up-form");
 
 signUpButton.addEventListener("click", () => {
-    if(signUpForm.style.display === "none") {
+    const computedStyle = window.getComputedStyle(signUpForm);
+    if(computedStyle.display === "none") {
         signUpForm.style.display = "block";
     } else {
         signUpForm.style.display = "none";
